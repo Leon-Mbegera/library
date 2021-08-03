4211c904dce915afbcea8f77c081f5aa0325755b
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-08-03 18:38:26 +0300

    Update function to conform to ES6 js

--- library.js
+++ library.js
@@ -1,8 +1,8 @@
 let myLibrary = [];
 
 function Book(title, author, pages, readStatus) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.readStatus = readStatus
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
 }
