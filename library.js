705779224b8dc799e380619e7ad898f47811c835
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-08-03 18:49:24 +0300

    Extract user's book input into storage

--- library.js
+++ library.js
@@ -1,8 +1,16 @@
-let myLibrary = [];
+const myLibrary = [];
 
-function Book(title, author, pages, readStatus) {
+const Book = (title, author, pages, readStatus)=> {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.readStatus = readStatus;
+}
+
+const addBookToLibrary =()=> {
+  event.preventDefault();
+  title = document.getElementById('bkTitle').value;
+  author = document.getElementById('bkAuthor').value; 
+  pages = document.getElementById('bkPages').value;
+  readStatus = document.getElementById('isRead').value;
 }
