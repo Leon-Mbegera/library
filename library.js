95e2c9a0200e69cda3643f208b8499f76505c864
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-08-04 19:38:45 +0300

    Fix eslint offenses

--- library.js
+++ library.js
@@ -7,6 +7,29 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus;
 }
 
+function readstatus(index, showBooks, bookCard) {
+  if (myLibrary[index].readStatus === false) {
+    myLibrary[index].readStatus = true;
+  } else {
+    myLibrary[index].readStatus = false;
+  }
+  showBooks(bookCard);
+}
+
+function removeFromLib(index, showBooks, bookCard) {
+  myLibrary.splice(index, 1);
+  showBooks(bookCard);
+}
+
+const bookSection = document.getElementById('bookSection');
+const showBooks = (bookCard) => {
+  bookSection.innerHTML = '';
+  myLibrary.forEach((newBook) => {
+    const newBookCard = bookCard(newBook);
+    bookSection.appendChild(newBookCard);
+  });
+};
+
 const bookCard = (newBook) => {
   const div = document.createElement('div');
   const title = document.createElement('p');
@@ -18,55 +41,27 @@ const bookCard = (newBook) => {
   const read = document.createElement('p');
   read.textContent = `Read?: ${newBook.readStatus}`;
   const remove = document.createElement('button');
-  remove.innerHTML = "Eject Book"
+  remove.innerHTML = 'Eject Book';
   const readStatus = document.createElement('button');
-  readStatus.innerHTML = "Toggle read status"
+  readStatus.innerHTML = 'Toggle read status';
   div.append(title, author, pages, read, remove, readStatus);
-  remove.addEventListener('click', ()=>removeFromLib(myLibrary.indexOf(newBook)) )
-  readStatus.addEventListener('click', ()=>readstatus(myLibrary.indexOf(newBook)) )
-  div.className = 'card'
-  return div
-}
+  remove.addEventListener('click', () => removeFromLib(myLibrary.indexOf(newBook), showBooks, bookCard));
+  readStatus.addEventListener('click', () => readstatus(myLibrary.indexOf(newBook), showBooks, bookCard));
+  div.className = 'card';
+  return div;
+};
 
-const bookSection = document.getElementById('bookSection');
-const showBooks = ()=> {
-  bookSection.innerHTML = "";
-  myLibrary.forEach((newBook)=> {
-    const newBookCard = bookCard(newBook);
-    bookSection.appendChild(newBookCard);
-     
-  });
-}
-
-function readstatus(index) {
-  if (myLibrary[index].readStatus === false) {
-    myLibrary[index].readStatus = true;
-  }
-  else {
-     myLibrary[index].readStatus = false; 
-  }
-  showBooks();
-}
-
-function removeFromLib(index) {
-  myLibrary.splice(index, 1)
-    showBooks();
-}
-
-const addBookToLibrary = (event)=> {
+const addBookToLibrary = (event, bookCard) => {
   event.preventDefault();
   const title = document.getElementById('bkTitle').value;
-  const author = document.getElementById('bkAuthor').value; 
+  const author = document.getElementById('bkAuthor').value;
   const pages = document.getElementById('bkPages').value;
   const readStatus = document.getElementById('isRead').checked;
 
-  const newBook = new Book(title, author, pages, readStatus)
+  const newBook = new Book(title, author, pages, readStatus);
   myLibrary.push(newBook);
-  showBooks();
-}
+  showBooks(bookCard);
+};
 
 const inputForm = document.getElementById('bkForm');
-inputForm.addEventListener('submit', addBookToLibrary);
-
-
-
+inputForm.addEventListener('submit', (event) => addBookToLibrary(event, bookCard));
